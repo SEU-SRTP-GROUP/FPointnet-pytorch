1a774563bab066b466b1fcd8fc2f55d8cae8e72a
chonepieceyb <1759315491@qq.com>
2020-02-27 11:45:04 +0800

    完成 3d box 分割模块

--- model/frustum_pointnets_v1.py
+++ model/frustum_pointnets_v1.py
@@ -2,13 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import os
+import sys
+from collections import OrderedDict
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+from model_util import NUM_HEADING_BIN, NUM_SIZE_CLUSTER, NUM_OBJECT_POINT
 
 class Config(object):
     def __init__(self):
         '''
         说明： 配置类，保存着 FPointNet所需要的所有参数，如果需要参数就在Config类的构造函数里面加,参数用大写加下划线表示 eg: IS_TRAINING, BN_DECAY
         '''
-
+        self.IS_TRAINING = True           # 是否训练 bn 层 在eval的时候应该为 false
+        self.BN_DECAY = 0.9               # bn 层 的 momentum 参数
 class FPointNet(nn.Module):
     def __init__(self,config):
         '''
@@ -18,14 +27,56 @@ class FPointNet(nn.Module):
         super(FPointNet,self).__init__()
         self.config = config
         self.end_points = {}             #所有输出构成的字典
-
+        self.conv_3dbox_1 = nn.Sequential(OrderedDict([
+            ('conv_3dbox_1', nn.Conv2d(3,128,[1,1])),
+            ('bn_3dbox_1', nn.BatchNorm2d(128,momentum=self.config.BN_DECAY,affine=self.config.IS_TRAINING))
+            ('relu_3dbox_1', nn.ReLU()),
+        ]))
+        self.conv_3dbox_2 = nn.Sequential(OrderedDict([
+            ('conv_3dbox_2', nn.Conv2d(128, 128, [1, 1])),
+            ('bn_3dbox_2', nn.BatchNorm2d(128, momentum=self.config.BN_DECAY, affine=self.config.IS_TRAINING))
+            ('relu_3dbox_2', nn.ReLU()),
+        ]))
+        self.conv_3dbox_3 = nn.Sequential(OrderedDict([
+            ('conv_3dbox_3', nn.Conv2d(128, 256, [1, 1])),
+            ('bn_3dbox_3', nn.BatchNorm2d(256, momentum=self.config.BN_DECAY, affine=self.config.IS_TRAINING))
+            ('relu_3dbox_3', nn.ReLU()),
+        ]))
+        self.conv_3dbox_4 = nn.Sequential(OrderedDict([
+            ('conv_3dbox_4', nn.Conv2d(256, 512, [1, 1])),
+            ('bn_3dbox_4', nn.BatchNorm2d(512, momentum=self.config.BN_DECAY, affine=self.config.IS_TRAINING))
+            ('relu_3dbox_4', nn.ReLU()),
+        ]))
+        self.fc_3dbox_1 = nn.Sequential(OrderedDict([
+            ('fc_3dbox_1', nn.Linear(515,512)),
+            ('bn_3dbox_5', nn.BatchNorm2d(512, momentum=self.config.BN_DECAY, affine=self.config.IS_TRAINING))
+            ('relu_3dbox_5', nn.ReLU()),
+        ]))
+        self.fc_3dbox_2 = nn.Sequential(OrderedDict([
+            ('fc_3dbox_2', nn.Linear(515, 256)),
+            ('bn_3dbox_6', nn.BatchNorm2d(256, momentum=self.config.BN_DECAY, affine=self.config.IS_TRAINING))
+            ('relu_3dbox_6', nn.ReLU()),
+        ]))
+        self.fc_3dbox_3 = nn.Linear(256, 3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER*4)
+        
     def get_3d_box_estimation_v1_net(self,object_point_cloud,one_hot_vec):
         '''
         3D box 回归模块
         @author :chonepieceyb
-        :param object_point_cloud:  经过上一个模块处理后的点云数据  shape: (batch,Mask_num,C)  point clouds in object coordinate
+        :param object_point_cloud:  经过上一个模块处理后的点云数据  shape: (batch,C,Mask_num)  point clouds in object coordinate chanels should be 3
         :param one_hot_vec:  tensor in shape （batch,3) , length-3 vectors indicating predicted object type
         :return:  tensor in shape (B,3+NUM_HEADING_BIN*2+NUM_SIZE_CLUSTER*4) including box centers, heading bin class scores and residuals, and size cluster scores and residuals
         '''
-        num_point = object_point_cloud.size()[1]
-        net = torch.unsqueeze(object_point_cloud,2)         # change shape to (batch,M,1,C)
+        num_point = object_point_cloud.size()[2]
+        net = torch.unsqueeze(object_point_cloud,3)         # change shape to (batch,C,M,1)
+        net = self.conv_3dbox_1(net)                        # conv_block conv+bn+relu ,  [B,3,M,1]->[B,128,M,1]
+        net = self.conv_3dbox_2(net)                        # conv_block conv+bn+relu ,  [B,128,M,1]->[B,128,M,1]
+        net = self.conv_3dbox_3(net)                        # conv_block conv+bn+relu ,  [B,128,M,1]->[B,256,M,1]
+        net = self.conv_3dbox_4(net)                        # conv_block conv+bn+relu ,  [B,256,M,1]->[B,512,M,1]
+        net = F.max_pool2d(net,(num_point,1))               # max_pool layer   [B,512,M,1]->[B,512,1,1]
+        net = net.view(-1,512)                     # change shape to [B,512]
+        net = torch.cat((net,one_hot_vec),dim=1)            #  shape  [B,512+3]
+        net = self.fc_3dbox_1(net)
+        net = self.fc_3dbox_2(net)
+        output = self.fc_3dbox_3(net)
+        return output
--- model/model_util.py
+++ model/model_util.py
@@ -2,3 +2,25 @@ import numpy as np
 import torch
 import os
 import sys
+
+NUM_HEADING_BIN = 12
+NUM_SIZE_CLUSTER = 8 # one cluster for each type 每种类型一个集群
+NUM_OBJECT_POINT = 512
+g_type2class={'Car':0, 'Van':1, 'Truck':2, 'Pedestrian':3,
+              'Person_sitting':4, 'Cyclist':5, 'Tram':6, 'Misc':7}
+g_class2type = {g_type2class[t]:t for t in g_type2class}#与上面相反，数字对应字符串
+g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
+#np.array,存储单一数据类型的多维数组,避免浪费内存和CPU。
+g_type_mean_size = {'Car': np.array([3.88311640418,1.62856739989,1.52563191462]),
+                    'Van': np.array([5.06763659,1.9007158,2.20532825]),
+                    'Truck': np.array([10.13586957,2.58549199,3.2520595]),
+                    'Pedestrian': np.array([0.84422524,0.66068622,1.76255119]),
+                    'Person_sitting': np.array([0.80057803,0.5983815,1.27450867]),
+                    'Cyclist': np.array([1.76282397,0.59706367,1.73698127]),
+                    'Tram': np.array([16.17150617,2.53246914,3.53079012]),
+                    'Misc': np.array([3.64300781,1.54298177,1.92320313])}
+g_mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3)) # size clustrs
+#array([[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.],[ 0.,  0.,  0.]])
+for i in range(NUM_SIZE_CLUSTER):
+    g_mean_size_arr[i,:] = g_type_mean_size[g_class2type[i]]
+    #copy
