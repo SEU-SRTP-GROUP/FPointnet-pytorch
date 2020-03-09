58d315a20d830e017e10769759fc457ed39c25b6
chonepieceyb <1759315491@qq.com>
2020-03-09 22:29:21 +0800

    训练大体框架

---
+++ train/train_39.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import sys
+from datetime import datetime
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import model
+import provider
+
+BASE_DIR = os.path.dirname(__file__)
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
+parser.add_argument('--num_point', type=int, default=2048, help='Point Number [default: 2048]')
+parser.add_argument('--max_epoch', type=int, default=201, help='Epoch to run [default: 201]')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
+parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
+parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
+parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
+parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.7]')
+parser.add_argument('--no_intensity', action='store_true', help='Only use XYZ for training')
+parser.add_argument('--restore_model_path', default=None, help='Restore model path e.g. log/model.ckpt [default: None]')
+# parse_args() 传递一组参数字符串来解析命令行，返回一个命名空间包含传递给命令的参数
+FLAGS = parser.parse_args()
+
+#训练参数
+EPOCH_CNT = 0
+# batch_size表明这个batch中包含多少个点云数据
+BATCH_SIZE = FLAGS.batch_size
+# num_point表明每个点云中含有多少个点
+NUM_POINT = FLAGS.num_point
+MAX_EPOCH = FLAGS.max_epoch
+BASE_LEARNING_RATE = FLAGS.learning_rate
+GPU_INDEX = FLAGS.gpu
+MOMENTUM = FLAGS.momentum
+OPTIMIZER = FLAGS.optimizer
+DECAY_STEP = FLAGS.decay_step
+#   初始时使用较大的学习率较快地得到较优解，随着迭代学习率呈指数逐渐减小。
+#  decayed_learning_rate = learning_rate*(decay_rate^(global_steps/decay_steps)
+DECAY_RATE = FLAGS.decay_rate
+NUM_CHANNEL = 3 if FLAGS.no_intensity else 4 # point feature channel
+NUM_CLASSES = 2 # segmentation has two classes
+LOG_DIR = FLAGS.log_dir                      #训练结果log的路径
+
+if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
+LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
+LOG_FOUT.write(str(FLAGS)+'\n')
+
+################################网络参数##################################
+
+BN_INIT_DECAY = 0.5
+BN_DECAY_DECAY_RATE = 0.5
+BN_DECAY_DECAY_STEP = float(DECAY_STEP)
+BN_DECAY_CLIP = 0.99
+
+# Load Frustum Datasets. Use default data paths.
+TRAIN_DATASET = provider.FrustumDataset(npoints=NUM_POINT, split='train',
+    rotate_to_center=True, random_flip=True, random_shift=True, one_hot=True)
+TEST_DATASET = provider.FrustumDataset(npoints=NUM_POINT, split='val',
+    rotate_to_center=True, one_hot=True)
+
+def log_string(out_str):
+    LOG_FOUT.write(out_str+'\n')
+    LOG_FOUT.flush()
+    print(out_str)
+
+def get_learning_rate(batch):
